882c5e92a23dd32e05aa730749609d67458ef511
bhargav473 <bhargav.473@gmail.com>
2022-07-06 19:07:12 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
 "use strict";
+
+ // Disable right-click
+ document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+ function ctrlShiftKey(e, keyCode) {
+   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+ }
+ 
+ document.onkeydown = (e) => {
+   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+   if (
+	 event.keyCode === 123 ||
+	 ctrlShiftKey(e, 'I') ||
+	 ctrlShiftKey(e, 'J') ||
+	 ctrlShiftKey(e, 'C') ||
+	 (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+   )
+	 return false;
+ };
+ 
+ 
+ function ctrlShiftKey(e,t){return e.ctrlKey&&e.shiftKey&&e.keyCode===t.charCodeAt(0)}document.addEventListener("contextmenu",e=>e.preventDefault()),document.onkeydown=(e=>{if(123===event.keyCode||ctrlShiftKey(e,"I")||ctrlShiftKey(e,"J")||ctrlShiftKey(e,"C")||e.ctrlKey&&e.keyCode==="U".charCodeAt(0))return!1});
+
+
+
 (function () {
 	// Global variables
 	let
